77bfe3f65486d7a8ea9b68b984c4fffa899da491
saveetha17 <saveethakathiravan@gmail.com>
2023-05-27 08:19:18 -0400

    made a Triangle

--- library/shape.js
+++ library/shape.js
@@ -8,4 +8,10 @@ class Shape {
   }
 }
 
+class Trriangle extends Shape {
+    render(){
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+    }
+}
+
 module.exports = Shape;
