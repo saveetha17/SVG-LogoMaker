7f952c8f4d77dfed237aeb1824a8ce8693b770ce
saveetha17 <saveethakathiravan@gmail.com>
2023-05-27 08:23:10 -0400

    made a Square

--- library/shape.js
+++ library/shape.js
@@ -8,9 +8,15 @@ class Shape {
   }
 }
 
-class Trriangle extends Shape {
+class Triangle extends Shape {
     render(){
         return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+    }
+}
+
+class Square extends Shape {
+    render() {
+        return `<rect x="80" y="50" width="105" height="105" fill="${this.color}" />`;
     }
 }
 
