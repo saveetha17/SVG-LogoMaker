8aeca1590a7eb8e8e4959521437784c77bfd042d
saveetha17 <saveethakathiravan@gmail.com>
2023-05-27 08:34:25 -0400

    updated svg file

--- library/svg.js
+++ library/svg.js
@@ -3,4 +3,10 @@ class SVG {
         this.text="";
         this.shape="";
     }
+    render() {
+        return `<svg version="1.1" width="350" height="250" xmlns="http://www.w3.org/2000/svg">${this.shape}${this.text}</svg>`;
+      }    
 }
+
+
+module.exports = SVG;
