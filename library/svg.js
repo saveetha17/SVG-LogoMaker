ef5936c658ff4d9577ee64b613df2b11f422a79d
saveetha17 <saveethakathiravan@gmail.com>
2023-05-27 08:36:44 -0400

    updated1 svg file

--- library/svg.js
+++ library/svg.js
@@ -6,6 +6,14 @@ class SVG {
     render() {
         return `<svg version="1.1" width="350" height="250" xmlns="http://www.w3.org/2000/svg">${this.shape}${this.text}</svg>`;
       }    
+
+      setText(msg, color) {
+        if (msg.length > 3) {
+          throw new Error("Text must not exceed 3 characters.");
+        }
+        this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${msg}</text>`;
+      }
+    
 }
 
 
